ef0d08b5fd70859978886afd78e1b4ae3f51c68e
ArHiFranki <franki550@gmail.com>
2022-01-21 14:02:58 +0700

    Задал границы для перемещения игрока

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,18 +6,42 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 10f;
-
     private Vector2 rawInput;
+
+    [SerializeField] private float paddingLeft;
+    [SerializeField] private float paddingRight;
+    [SerializeField] private float paddingTop;
+    [SerializeField] private float paddingBottom;
+
+    private Vector2 minBounds;
+    private Vector2 maxBounds;
+
+    private void Start()
+    {
+        InitBounds();
+    }
 
     private void Update()
     {
         PlayerMove();
     }
 
+    private void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
     private void PlayerMove()
     {
-        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+
+        Vector2 newPosition = new Vector2();
+        newPosition.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPosition.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingBottom, maxBounds.y - paddingTop);
+
+        transform.position = newPosition;
     }
 
     private void OnMove(InputValue value)
