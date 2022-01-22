a3fbb7cd7d212bcddb269469b5594d0211606ce7
ArHiFranki <franki550@gmail.com>
2022-01-22 21:44:57 +0700

    Добавил противникам возможность стрельбы с настраиваемыми параметрами

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,18 +1,31 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    [Header("General")]
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private float projectileSpeed = 10f;
     [SerializeField] private float projectileLifetime = 5f;
-    [SerializeField] private float firingRate = 0.2f;
+    [SerializeField] private float baseFiringRate = 0.2f;
 
-    public bool isFiring;
+    [Header("AI")]
+    [SerializeField] private bool useAI;
+    [SerializeField] private float firingRateVariance = 0f;
+    [SerializeField] private float minimumFiringRate = 0.1f;
+
+    [HideInInspector] public bool isFiring;
 
     private Coroutine firingCoroutine;
+
+    private void Start()
+    {
+        if (useAI)
+        {
+            isFiring = true;
+        }
+    }
 
     private void Update()
     {
@@ -45,7 +58,12 @@ public class Shooter : MonoBehaviour
             }
 
             Destroy(inctance, projectileLifetime);
-            yield return new WaitForSeconds(firingRate);
+
+            float timeToNextProjectile = Random.Range(baseFiringRate - firingRateVariance,
+                                                      baseFiringRate + firingRateVariance);
+            timeToNextProjectile = Mathf.Clamp(timeToNextProjectile, minimumFiringRate, float.MaxValue);
+
+            yield return new WaitForSeconds(timeToNextProjectile);
         }
     }
 }
