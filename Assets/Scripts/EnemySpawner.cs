a306f1173f43274c5790bbf7f8a6bcb21e7a9d77
ArHiFranki <franki550@gmail.com>
2022-01-22 12:55:56 +0700

    Добавил возможность бесконечного спавна волн

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -7,6 +7,7 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private List<WaveConfigSO> waveConfigs;
     [SerializeField] private float timeBetweenWaves = 0f;
+    [SerializeField] private bool isLooping;
     
     private WaveConfigSO currentWave;
 
@@ -22,20 +23,24 @@ public class EnemySpawner : MonoBehaviour
 
     private IEnumerator SpawnEnemyWaves()
     {
-        foreach (WaveConfigSO wave in waveConfigs)
+        do
         {
-            currentWave = wave;
-
-            for (int i = 0; i < currentWave.GetenemyCount(); i++)
+            foreach (WaveConfigSO wave in waveConfigs)
             {
-                Instantiate(currentWave.GetEnemyPrefab(i),
-                            currentWave.GetStartingWaypoint().position,
-                            Quaternion.identity,
-                            transform);
-                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
-            }
+                currentWave = wave;
 
-            yield return new WaitForSeconds(timeBetweenWaves);
+                for (int i = 0; i < currentWave.GetenemyCount(); i++)
+                {
+                    Instantiate(currentWave.GetEnemyPrefab(i),
+                                currentWave.GetStartingWaypoint().position,
+                                Quaternion.identity,
+                                transform);
+                    yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                }
+
+                yield return new WaitForSeconds(timeBetweenWaves);
+            }
         }
+        while (isLooping);
     }
 }
